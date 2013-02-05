3d35b06dba44e05517b7fd4a77cfd831099832bb
Dima Salakhov <dmitry.salakhov@doneright.ru>
2013-02-05 18:20:26 +0600

    Add Merge and Substitution for json

---
+++ PickAndRoll/Tests/EvalTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using PickAndRoll;
+using Xunit;
+
+namespace Tests
+{
+    public class EvalTests
+    {
+        private Configurator _configurator;
+
+        public EvalTests()
+        {
+            _configurator = new Configurator();
+        }
+
+        [Fact]
+        public void ReturnSameDictionaryIfValuesHaveNotParameters()
+        {
+            var config = new Dictionary<string, string>
+                                                    {
+                                                        {"a","1"},
+                                                        {"b","2"}
+                                                    };
+            var evaledConfig = _configurator.Eval(config);
+
+            Assert.Equal(config, evaledConfig);
+        }
+
+        [Fact]
+        public void ReturnDictionaryWithReplacedParameter()
+        {
+            var expected = new Dictionary<string, string>
+                                                    {
+                                                        {"a","1"},
+                                                        {"b","1"}
+                                                    };
+
+            var config = new Dictionary<string, string>
+                                                    {
+                                                        {"a","1"},
+                                                        {"b","@{a}"}
+                                                    };
+
+            Assert.Equal(expected, _configurator.Eval(config));
+        }
+
+        [Fact]
+        public void ReturnDictionaryWithReplacedParameters()
+        {
+            var expected = new Dictionary<string, string>
+                                                    {
+                                                        {"a","1"},
+                                                        {"c", "a:1 in b:(b is 1) with a=1"},
+                                                        {"b","(b is 1)"},
+                                                    };
+
+            var config = new Dictionary<string, string>
+                                                    {
+                                                        {"a","1"},
+                                                        {"c", "a:@{a} in b:@{b} with a=@{a}"},
+                                                        {"b","(b is @{a})"},
+                                                    };
+
+            Assert.Equal(expected, _configurator.Eval(config));
+        }
+    }
+}
---
+++ PickAndRoll/Tests/MergeTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using PickAndRoll;
+using Xunit;
+
+namespace Tests
+{
+    public class MergeTests
+    {
+        private readonly Configurator _configurator;
+
+        public MergeTests()
+        {
+            _configurator = new Configurator();
+        }
+
+        [Fact]
+        public void ThrowsException_IfInputFilesWereNotFound()
+        {
+            Assert.Throws<ArgumentNullException>(() => { _configurator.Merge(null); });
+        }
+
+        [Fact]
+        public void ReturnsDictionaryWithJson_ForOneFlatJson()
+        {
+            var expected = new Dictionary<string, string>()
+                                                      {
+                                                          {"one", "1"},
+                                                          {"two", "2"}
+                                                      };
+
+            const string json1 = "{" +
+                                 "\"one\":\"1\"," +
+                                 "\"two\":\"2\"" +
+                                 "}";
+
+            Assert.Equal(expected, _configurator.Merge(new []{json1}));
+        }
+
+        [Fact]
+        public void ReturnMergedOfMultipleDictionaries_ValuesOfLastDictionaryOverridesEarlierDictionaries()
+        {
+            var expected = new Dictionary<string, string>()
+                                     {
+                                         {"one.a", "1a"},
+                                         {"one.b", "1b"},
+                                         {"two", "22"},
+                                         {"one", "1"}
+                                     };
+
+            const string json1 = "{" +
+                                 "\"one.a\": \"1a\"," +
+                                 "\"one.b\": \"1b\"," +
+                                 "\"two\": \"2\"" +
+                                 "}";
+            const string json2 = "{" +
+                                 "\"one\":\"1\"," +
+                                 "\"two\":\"22\"" +
+                                 "}";
+
+            Assert.Equal(expected, _configurator.Merge(new[] { json1, json2 }));
+        }
+
+        [Fact(Skip = "For future release")]
+        public void ReturnsDictionaryWithJson_ForOneComplexJson()
+        {
+            var expected = new Dictionary<string, string>()
+                               {
+                                   {"one.a", "1a"},
+                                   {"one.b", "1b"},
+                                   {"two", "2"}
+                               };
+
+            const string file = "{" +
+                                "\"one\": {" +
+                                        "\"a\":\"1a\", " +
+                                        "\"b\":\"1b\"}," +
+                                "\"two\":\"2\"" +
+                                "}";
+
+            Assert.Equal(expected, _configurator.Merge(new[] { file }));
+        }
+
+        private string ReadResourseFile(string fileName)
+        {
+            var fileStream = GetType().Assembly.GetManifestResourceStream(fileName);
+            using (var reader = new StreamReader(fileStream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
